30e3fbe52997f5cd6f9da8226e0858e7cef7e037
Marcel Gotsch <gotschmarcel@me.com>
2016-04-03 18:39:18 +0200

    Move handlers into separate file

---
+++ handlers.go
@@ -0,0 +1,57 @@
+// Copyright 2016 Marcel Gotsch. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package goserv
+
+type ErrorHandler interface {
+	ServeHTTP(ResponseWriter, *Request, error)
+}
+
+type ErrorHandlerFunc func(ResponseWriter, *Request, error)
+
+func (e ErrorHandlerFunc) ServeHTTP(res ResponseWriter, req *Request, err error) {
+	e(res, req, err)
+}
+
+type Handler interface {
+	ServeHTTP(ResponseWriter, *Request)
+}
+
+type HandlerFunc func(ResponseWriter, *Request)
+
+func (h HandlerFunc) ServeHTTP(res ResponseWriter, req *Request) {
+	h(res, req)
+}
+
+type ParamHandler interface {
+	ServeHTTP(ResponseWriter, *Request, string)
+}
+
+type ParamHandlerFunc func(ResponseWriter, *Request, string)
+
+func (p ParamHandlerFunc) ServeHTTP(res ResponseWriter, req *Request, value string) {
+	p(res, req, value)
+}
+
+type pathHandler interface {
+	serveHTTP(ResponseWriter, *Request)
+	match(path string) bool
+	parseParams(path string) Params
+}
+
+type middlewareHandler struct {
+	handler Handler
+}
+
+func (m *middlewareHandler) serveHTTP(res ResponseWriter, req *Request) {
+	m.handler.ServeHTTP(res, req)
+}
+
+func (m *middlewareHandler) match(path string) bool {
+	return true
+}
+
+func (m *middlewareHandler) parseParams(path string) Params {
+	return nil
+}
--- types.go
+++ types.go
@@ -9,39 +9,3 @@ type Locals interface{}
 type Params map[string]string
 
 func (p Params) Get(key string) string { return p[key] }
-
-type ErrorHandler func(ResponseWriter, *Request, error)
-
-type Handler interface {
-	ServeHTTP(ResponseWriter, *Request)
-}
-
-type HandlerFunc func(ResponseWriter, *Request)
-
-func (h HandlerFunc) ServeHTTP(res ResponseWriter, req *Request) {
-	h(res, req)
-}
-
-type ParamHandler func(ResponseWriter, *Request, string)
-
-type internalHandler interface {
-	serveHTTP(ResponseWriter, *Request)
-	match(path string) bool
-	params(path string) Params
-}
-
-type middlewareHandler struct {
-	handler Handler
-}
-
-func (m *middlewareHandler) serveHTTP(res ResponseWriter, req *Request) {
-	m.handler.ServeHTTP(res, req)
-}
-
-func (m *middlewareHandler) match(path string) bool {
-	return true
-}
-
-func (m *middlewareHandler) params(path string) Params {
-	return nil
-}
