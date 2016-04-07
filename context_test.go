91394e6505dac1151a43a438a7bfae29e3b3d1d4
Marcel Gotsch <gotschmarcel@me.com>
2016-04-07 12:41:52 +0200

    Add simple unit test for Context

---
+++ context_test.go
@@ -0,0 +1,35 @@
+// Copyright 2016 Marcel Gotsch. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package goserv
+
+import "testing"
+
+func TestContext(t *testing.T) {
+	c := Context{}
+
+	// Internal storage still nil.
+	if c.Exists("key") {
+		t.Error("The key 'key' should not exist in context")
+	}
+
+	if c.Get("key") != nil {
+		t.Error("Expected nil value for 'key'")
+	}
+
+	c.Set("key", "value")
+
+	// Internal storage created.
+	if !c.Exists("key") {
+		t.Error("Expected 'key' to exist")
+	}
+
+	if c.Get("key").(string) != "value" {
+		t.Error("Expected 'key' to be 'value'")
+	}
+
+	if c.Get("key2") != nil {
+		t.Error("Expected nil value for 'key2'")
+	}
+}
