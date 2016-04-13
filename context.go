1551a9df1b795047175e307ac421c8ebcbaf3175
Marcel Gotsch <gotschmarcel@me.com>
2016-04-13 23:26:41 +0200

    Document Context

--- context.go
+++ context.go
@@ -4,22 +4,28 @@
 
 package goserv
 
+// Context stores key-value pairs supporting all data types.
 type Context struct {
 	store map[string]interface{}
 }
 
+// Set sets the value for the specified the key. It replaces any existing values.
 func (c *Context) Set(key string, value interface{}) {
 	c.store[key] = value
 }
 
+// Get retrieves the value for key. If the key doesn't exist in the Context,
+// Get returns nil.
 func (c *Context) Get(key string) interface{} {
 	return c.store[key]
 }
 
+// Delete deletes the value associated with key. If the key doesn't exist nothing happens.
 func (c *Context) Delete(key string) {
 	delete(c.store, key)
 }
 
+// Exists returns true if the specified key exists in the Context, otherwise false is returned.
 func (c *Context) Exists(key string) bool {
 	_, exists := c.store[key]
 	return exists
