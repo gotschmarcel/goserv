4c0a58c26e64af674b625a6340ab66030f8afb3b
Marcel Gotsch <gotschmarcel@me.com>
2016-04-13 23:32:58 +0200

    Document Request

--- request.go
+++ request.go
@@ -8,17 +8,31 @@ import (
 	"net/http"
 )
 
+// A Request represents an HTTP request received by the Server.
+//
+// It embeds the native http.Request, thus all native fields are still available
+// through Request. Every Request has it's own Context providing a key-value store to share
+// data between multiple Handlers. In case that the Route handling the Request has parameters, the parameter
+// values are extracted from the Request's path and stored in .Params.
 type Request struct {
+	// Embedded http.Request.
 	*http.Request
-	Context       *Context
-	Params        Params
+
+	// Request specific key-value store to share data between Handlers
+	Context *Context
+
+	// Key-value store containing named parameter values extracted from
+	// the Request's path. See Route.
+	Params Params
+
 	sanitizedPath string
 }
 
+// SanitizedPath returns the Request's path sanitized with SanitizePath().
 func (r *Request) SanitizedPath() string {
 	return r.sanitizedPath
 }
 
 func newRequest(r *http.Request) *Request {
-	return &Request{r, newContext(), nil, make(Params), SanitizePath(r.URL.Path)}
+	return &Request{r, newContext(), make(Params), SanitizePath(r.URL.Path)}
 }
