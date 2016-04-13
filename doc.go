99ff0181f43e3e91e69dc5bfd630bb7cdb0a39d7
Marcel Gotsch <gotschmarcel@me.com>
2016-04-13 23:26:51 +0200

    Document package

--- doc.go
+++ doc.go
@@ -2,5 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// Package goserv provides a fast, easy and minimalistic framework for
+// web applications in Go.
+//
 //
 package goserv
